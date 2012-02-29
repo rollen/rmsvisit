9da5e437e7a2b6ae19c4a170db8f255b51353976
Rollen Gomes <avelliance@gmail.com>
2012-02-29 11:42:07 +0800

    add source for posters onto resource page

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,8 +3,8 @@ class HomesController < ApplicationController
   end
 
   def resources
-    @posters = [{low: 'a_free_digital_societey_low.jpg', high: 'a_free_digital_societey.jpg'}, 
-                {low: 'free_software_and_your_digital_freedom_low.jpg', high: 'free_software_and_your_digital_freedom_.jpg'}]
+    @posters = [{source: 'a_free_digital_societey.xcf', low: 'a_free_digital_societey_low.jpg', high: 'a_free_digital_societey.jpg'}, 
+                {source: 'free_software_and_your_digital_freedom.xcf', low: 'free_software_and_your_digital_freedom_low.jpg', high: 'free_software_and_your_digital_freedom_.jpg'}]
   end
 
   def contact
