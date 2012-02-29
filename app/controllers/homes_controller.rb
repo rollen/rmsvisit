3bbd9f7b70e07569d540d2dc79f454b6aff4ab31
Rollen Gomes <avelliance@gmail.com>
2012-02-29 11:21:24 +0800

    refactor poster view to use data from posters hash

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,6 +3,7 @@ class HomesController < ApplicationController
   end
 
   def resources
+    @posters = [{name: 'a_free_digital_societey.jpg'}, {name: 'free_software_and_your_digital_freedom.jpg'}]
   end
 
   def contact
