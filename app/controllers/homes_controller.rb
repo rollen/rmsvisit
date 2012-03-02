561d480426193f94dcd59ebb72955b048f1e05a3
Rollen Gomes <avelliance@gmail.com>
2012-03-02 11:26:49 +0800

    switch order of posters

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,8 +3,7 @@ class HomesController < ApplicationController
   end
 
   def resources
-    @posters = [{source: 'a_free_digital_societey.xcf', low: 'a_free_digital_societey_low.jpg', high: 'a_free_digital_societey.jpg'}, 
-                {source: 'free_software_and_your_digital_freedom.xcf', low: 'free_software_and_your_digital_freedom_low.jpg', high: 'free_software_and_your_digital_freedom_.jpg'}]
+    @posters = [{source: 'free_software_and_your_digital_freedom.xcf', low: 'free_software_and_your_digital_freedom_low.jpg', high: 'free_software_and_your_digital_freedom_.jpg'},{source: 'a_free_digital_societey.xcf', low: 'a_free_digital_societey_low.jpg', high: 'a_free_digital_societey.jpg'}]
   end
 
   def contact
