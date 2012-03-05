05fa0d18e5b322505edd73e839ccba81cffe4408
Rollen Gomes <avelliance@gmail.com>
2012-03-05 23:42:09 +0800

    add pdf posters of both events in sizes A3 and A2

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,7 @@ class HomesController < ApplicationController
   end
 
   def resources
-    @posters = [{source: 'free_software_and_your_freedom.xcf', low: 'free_software_and_your_freedom_low.jpg', high: 'free_software_and_your_freedom.jpg'},{source: 'a_free_digital_society.xcf', low: 'a_free_digital_society_low.jpg', high: 'a_free_digital_society.jpg'}]
+    @posters = [{a3:'A3_free_software_and_your_freedom.pdf',a2:'A2_free_software_and_your_freedom.pdf', source: 'free_software_and_your_freedom.xcf', low: 'free_software_and_your_freedom_low.jpg', high: 'free_software_and_your_freedom.jpg'},{a2:'A2_a_free_digital_society.pdf', a3:'A3_a_free_digital_society.pdf', source: 'a_free_digital_society.xcf', low: 'a_free_digital_society_low.jpg', high: 'a_free_digital_society.jpg'}]
   end
 
   def contact
