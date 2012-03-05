09c9cc1a37d054868216871cdab9e3bb5859c092
Rollen Gomes <avelliance@gmail.com>
2012-03-05 15:05:23 +0800

    update names of posters to remove typos

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,7 @@ class HomesController < ApplicationController
   end
 
   def resources
-    @posters = [{source: 'free_software_and_your_digital_freedom.xcf', low: 'free_software_and_your_digital_freedom_low.jpg', high: 'free_software_and_your_digital_freedom_.jpg'},{source: 'a_free_digital_societey.xcf', low: 'a_free_digital_societey_low.jpg', high: 'a_free_digital_societey.jpg'}]
+    @posters = [{source: 'free_software_and_your_freedom.xcf', low: 'free_software_and_your_freedom_low.jpg', high: 'free_software_and_your_freedom.jpg'},{source: 'a_free_digital_society.xcf', low: 'a_free_digital_society_low.jpg', high: 'a_free_digital_society.jpg'}]
   end
 
   def contact
